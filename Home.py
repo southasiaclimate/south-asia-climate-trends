256193e0be86712f4738d9a041b7585a69352ef0
Climate Expert <nilofarzubair92@gmail.com>
2026-07-24 13:55:30 +0500

    Fix formatting of Google site verification meta tag

--- Home.py
+++ Home.py
@@ -13,8 +13,9 @@ st.set_page_config(
     layout="wide",
 )
 
+
 st.markdown(
-    '<meta name="google-site-verification" content="Ece2qBvMQmE8UNnCbMciBVPciJCcsyNtOBb6MF0QDIo" />,
+    '<meta name="google-site-verification" content="Ece2qBvMQmE8UNnCbMciBVPciJCcsyNtOBb6MF0QDIo" />',
     unsafe_allow_html=True,
 )
 
