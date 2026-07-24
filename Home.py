157b2c3ad5422eceffb25f346d1b45412917bf1c
Climate Expert <nilofarzubair92@gmail.com>
2026-07-24 13:26:50 +0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -13,10 +13,7 @@ st.set_page_config(
     layout="wide",
 )
 
-st.markdown(
-    '<meta name="google-site-verification" content="Ece2qBvMQmE8UNnCbMciBVPciJCcsyNtOBb6MF0QDIo" />',
-    unsafe_allow_html=True,
-)
+<meta name="google-site-verification" content="Ece2qBvMQmE8UNnCbMciBVPciJCcsyNtOBb6MF0QDIo" />
 
 # ---------------------------------------------------------------
 # Custom styling
